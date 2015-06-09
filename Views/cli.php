ce09a33b637b3d60a1a577e7c128a6e94ce07bea
shokora <t.strijdhorst@student.utwente.nl>
2015-06-09 16:58:53 +0200

    +Finished CLI View

---
+++ Views/cli.php
@@ -0,0 +1,19 @@
+<?php
+
+include('../Services/UnitaryGenerator.php');
+/**
+ * User: Tim Strijdhorst
+ * Project: Unitary Group Calculator
+ * Date: 9-6-15
+ * Time: 16:30
+ */
+
+if ($argc <= 1) {
+	die('Usage: '.$argv[0].' <N> to generate the output for U(N)');
+}
+
+$baseNumber = $argv[1];
+$generator = new \Services\UnitaryGenerator($baseNumber);
+$uGroup = $generator->generate();
+
+echo 'U('.$baseNumber.') = ('.implode($uGroup,',').')';
